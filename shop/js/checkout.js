dce181b4ea8d1116a20f43c2428a0b1fd90ed6ed
wldb789 <wldb789@gmail.com>
2026-03-31 19:12:40 +0900

    웹페이지 헤더 변경

--- shop/js/checkout.js
+++ shop/js/checkout.js
@@ -15,26 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const isLogin = localStorage.getItem("isLogin");
     const username = loginUser.name || loginUser.id || "";
 
-    const loginBtn = document.querySelector('a[href="login.html"]');
-    const signupBtn = document.querySelector('a[href="signup.html"]');
-    const nav = document.querySelector('.nav-links');
-
-    if (isLogin === "true" && username) {
-        if (loginBtn) loginBtn.style.display = "none";
-        if (signupBtn) signupBtn.style.display = "none";
-
-        const userLi = document.createElement("li");
-        userLi.innerHTML = `<span>${username}님</span>`;
-
-        const shopLi = document.querySelector('.nav-links li a[href="index.html"]').closest('li');
-
-        if (shopLi) {
-            nav.insertBefore(userLi, shopLi); // 👈 쇼핑하기 앞에 넣기
-        } else {
-            nav.appendChild(userLi);
-        }
-
-    }
+    
 
     // ✅ 결제페이지 배송정보 자동입력
     const buyerNameInput = document.querySelector('input[placeholder="이름"]');
